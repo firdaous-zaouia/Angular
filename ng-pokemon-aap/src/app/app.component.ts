2a0333fc5b4b52c7f9c8d255a4aa8e5ef02d0690
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-07-20 21:16:30 +0200

    create-details-liste-pokemon

--- ng-pokemon-aap/src/app/app.component.ts
+++ ng-pokemon-aap/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS} from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component} from '@angular/core';
 
 
 
@@ -8,24 +6,5 @@ import { Pokemon } from './pokemon';
   selector: 'app-root',
   templateUrl: './app.component.html'
   })
-export class AppComponent implements OnInit{
-  pokemonsList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit(){
-    console.table(this.pokemonsList);
-  }
-  selectPokemon(pokemonId: Pokemon){
-
-    const Pokemon: Pokemon|undefined = this.pokemonsList.find(pokemon => pokemon.id == +pokemonId);
-    if(Pokemon){
-      console.log(`vous avez cliqué sur le pokémon ${Pokemon.name}`);
-      this.pokemonSelected = Pokemon;
-    }else{
-      console.log(`vous avez cliqué sur le pokémon qui n'exixte pas.`);
-      this.pokemonSelected = Pokemon;
-      
-    }
-    
-  }
+export class AppComponent{
 }
