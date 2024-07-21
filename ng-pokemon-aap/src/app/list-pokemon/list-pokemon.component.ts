aafbe900f943f1982d791e8034cb6e848b8e3cf0
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-07-21 19:32:13 +0200

    create-component-error-404

--- ng-pokemon-aap/src/app/list-pokemon/list-pokemon.component.ts
+++ ng-pokemon-aap/src/app/list-pokemon/list-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component} from '@angular/core';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -8,4 +9,10 @@ import { Pokemon } from '../pokemon';
 })
 export class ListPokemonComponent  {
   pokemonsList: Pokemon[] = POKEMONS;
+
+  constructor(private router: Router){}
+
+  goToPokemon(pokemon: Pokemon){
+    this.router.navigate(['/pokemon',pokemon.id]);
+  }
 }
