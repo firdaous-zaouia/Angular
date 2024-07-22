ba4281daa3e11324c5174cafd6dd20cfdfb44293
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-07-22 06:16:42 +0200

    create nnew service

--- ng-pokemon-aap/src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ ng-pokemon-aap/src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,16 +1,23 @@
-import { Component} from '@angular/core';
-import { POKEMONS } from '../mock-pokemon-list';
-import { Pokemon } from '../pokemon';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import {PokemonService} from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
 })
-export class ListPokemonComponent  {
-  pokemonsList: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit {
+  pokemonsList: Pokemon[];
 
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService 
+    ){}
+
+    ngOnInit(){
+     this.pokemonsList= this.pokemonService.getPokemonList();
+    }
 
   goToPokemon(pokemon: Pokemon){
     this.router.navigate(['/pokemon',pokemon.id]);
---
+++ ng-pokemon-aap/src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
+
+@Injectable(
+  )
+export class PokemonService {
+
+getPokemonList(): Pokemon[]{
+  return POKEMONS;
+}
+
+getPkemonById(pokemonId: number): Pokemon|undefined{
+  return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+}
+
+getPokemonTypeList(): string[]{
+    return [
+      'plante',
+      'Feu', 
+      'Eau', 
+      'Insecte', 
+      'Normale', 
+      'Electrick', 
+      'Poisson', 
+      'Fée', 
+      'Vol', 
+      'Combat', 
+      'Psy'
+    ];
+  }
+}
