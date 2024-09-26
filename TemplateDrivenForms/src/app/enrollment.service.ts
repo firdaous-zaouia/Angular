4471ec02454d37992498b6077c0d5cfefb302422
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-09-26 11:30:52 +0200

    Ajout du code du serverjs dans enrollment.Service.ts

--- TemplateDrivenForms/src/app/enrollment.service.ts
+++ TemplateDrivenForms/src/app/enrollment.service.ts
@@ -14,3 +14,29 @@ export class EnrollmentService {
     return this._http.post<any>(this._url, user);
   }
 }
+
+
+// const express = require("express");
+// const bodyPaser = require("body-parser");
+// const cors = require("cors");
+
+// const PORT = 3000;
+
+// const app = express();
+
+// app.use(bodyPaser.json());
+
+// app.use(cors());
+
+// app.get("/", function (req, res) {
+//   res.send("Hello from server");
+// });
+
+// app.post("/enroll", function (req, res) {
+//   console.log(req.body);
+//   res.status(200).send({ message: "Data received" });
+// });
+
+// app.listen(PORT, function () {
+//   console.log("Server running on localhost:" + PORT);
+// });
