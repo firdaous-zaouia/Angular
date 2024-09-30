e28d5e19a1dc6fcb4047863cc0f7515cdf5c4766
Zaouia Firdaous <zaouia.firdaous@gmail.com>
2024-09-30 14:31:43 +0200

    Conditionnal validation to reactive forms

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/userName.validator';
 import { PasswordValidator } from './shared/password.validator';
 
@@ -8,24 +8,46 @@ import { PasswordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  registrationForm!: FormGroup;
 
   get userName(){
     return this.registrationForm.get('userName');
   }
 
+  get email(){
+    return this.registrationForm.get('email');
+  }
+
   constructor(private fb: FormBuilder){}
 
-  registrationForm = this.fb.group({
-    userName: ['',[Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/),forbiddenNameValidator(/admin/)]],
-    password: [''],
-    confirmPassword: [''],
-    adresse: this.fb.group({
-      city: [''],
-      State: [''],
-      postalcode:['']
+  ngOnInit(){
+    this.registrationForm = this.fb.group({
+      userName: ['',[Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/),forbiddenNameValidator(/admin/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      adresse: this.fb.group({
+        city: [''],
+        State: [''],
+        postalcode:['']
+      })
+    }, {validator: PasswordValidator});
+    
+    this.registrationForm.get('subscribe')?.valueChanges.subscribe(checkedvalue => {
+      const email =this.registrationForm.get('email');
+      if(checkedvalue){
+        email?.setValidators(Validators.required);
+      }else{
+        email?.clearValidators();
+      }
+      email?.updateValueAndValidity();
     })
-  }, {validator: PasswordValidator});
+  }
+
+  
 
   // registrationForm = new FormGroup({
   //   userName: new FormControl(''),
